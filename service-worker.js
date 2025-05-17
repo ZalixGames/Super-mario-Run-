938764ffd2d41b44c6c8fea6adff658bc51777a8
ZalixGames <7sstock009@gmail.com>
2025-05-17 06:58:36 -0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,74 @@
+const CACHE_NAME = 'mario-run-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap',
+  'https://www.transparenttextures.com/patterns/stardust.png',
+  'https://i.imgur.com/nxRZ03R.png',
+  'https://i.imgur.com/KFWJlte.png',
+  'https://imgur.com/QJZ5IJp.png'
+];
+
+// Install event - cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event - serve from cache, fall back to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached response if found
+        if (response) {
+          return response;
+        }
+
+        // Clone the request because it can only be used once
+        const fetchRequest = event.request.clone();
+
+        // Make network request and cache the response
+        return fetch(fetchRequest).then(
+          (response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response because it can only be used once
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+}); 
